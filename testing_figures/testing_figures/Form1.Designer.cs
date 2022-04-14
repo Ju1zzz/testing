55cfeca97eaa0152e3a75d5937560444bc150d2f
julia-oop <kuznetsova1yulia@yandex.ru>
2022-04-14 13:54:30 +0800

    all

---
+++ testing_figures/testing_figures/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace testing_figures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.errBottom = new System.Windows.Forms.Label();
+            this.errTop = new System.Windows.Forms.Label();
+            this.bottom = new System.Windows.Forms.TextBox();
+            this.top = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1066, 894);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(314, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите верхнее основание трапеции:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Info;
+            this.label2.Location = new System.Drawing.Point(13, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(311, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите нижнее основание трапеции:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Info;
+            this.label3.Location = new System.Drawing.Point(10, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(247, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Площадь закрашенной части:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(12, 327);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textBox3.BackColor = System.Drawing.Color.SteelBlue;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Location = new System.Drawing.Point(12, 286);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 7;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 374);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(321, 201);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.trackBar1.Location = new System.Drawing.Point(3, 605);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(430, 45);
+            this.trackBar1.TabIndex = 11;
+            this.trackBar1.Value = 20;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.errBottom);
+            this.panel1.Controls.Add(this.errTop);
+            this.panel1.Controls.Add(this.bottom);
+            this.panel1.Controls.Add(this.top);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Location = new System.Drawing.Point(1097, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(445, 901);
+            this.panel1.TabIndex = 12;
+            // 
+            // errBottom
+            // 
+            this.errBottom.AutoSize = true;
+            this.errBottom.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.errBottom.ForeColor = System.Drawing.Color.Maroon;
+            this.errBottom.Location = new System.Drawing.Point(13, 166);
+            this.errBottom.Name = "errBottom";
+            this.errBottom.Size = new System.Drawing.Size(0, 22);
+            this.errBottom.TabIndex = 15;
+            // 
+            // errTop
+            // 
+            this.errTop.AutoSize = true;
+            this.errTop.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.errTop.ForeColor = System.Drawing.Color.Maroon;
+            this.errTop.Location = new System.Drawing.Point(11, 71);
+            this.errTop.Name = "errTop";
+            this.errTop.Size = new System.Drawing.Size(0, 22);
+            this.errTop.TabIndex = 14;
+            // 
+            // bottom
+            // 
+            this.bottom.Location = new System.Drawing.Point(12, 126);
+            this.bottom.Name = "bottom";
+            this.bottom.Size = new System.Drawing.Size(142, 20);
+            this.bottom.TabIndex = 13;
+            this.bottom.TextChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // top
+            // 
+            this.top.Location = new System.Drawing.Point(13, 39);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(144, 20);
+            this.top.TabIndex = 12;
+            this.top.TextChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1542, 913);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Расчёт площади";
+            this.Resize += new System.EventHandler(this.button1_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox bottom;
+        private System.Windows.Forms.TextBox top;
+        private System.Windows.Forms.Label errBottom;
+        private System.Windows.Forms.Label errTop;
+    }
+}
+
---
+++ testing_figures/testing_figures/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testing_figures
+{
+    public partial class Form1 : Form
+    {
+        const double p = 37.936;
+        Point center = new Point();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+            center.X = pictureBox1.Width / 2;
+            center.Y = pictureBox1.Height / 2;          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            this.top.Text = "1";
+            this.top.Text = "1";
+        }
+        
+        void button1_Click(object sender, EventArgs e)
+        {
+            
+            double fe = checkNumber(top.Text);
+            double ad = checkNumber(bottom.Text);
+            if (fe == -2 )
+            {
+                label2.Visible = false;
+                bottom.Visible = false;
+                errTop.Visible = true;
+                errTop.Text = "Верхнее основание трапеции должно\n быть числом. \nНе целое число вводится с запятой";
+            } 
+            else if (fe == -1)
+            {
+                label2.Visible = false;
+                bottom.Visible = false;
+                errTop.Visible = true;
+                errTop.Text = "Верхнее основание трапеции должно быть \n в диапазоне от 0 до 100";
+            }
+            else if (ad == -2 )
+            {
+                label2.Visible = true;
+                bottom.Visible = true;
+                errTop.Visible = false;
+                errBottom.Text = "Нижнее основание трапеции должно\n быть числом. \nНе целое число вводится с запятой";
+            }
+            else if (ad == -1)
+            {
+                label2.Visible = true;
+                bottom.Visible = true;
+                errTop.Visible = false;
+                errBottom.Text = "Нижнее основание трапеции должно быть \n в диапазоне от 0 до 100 \n";
+            }
+            else if (fe == 0 || ad == 0)
+            {
+                textBox3.Text = "0 см";
+            }
+            else
+            {
+                label2.Visible = true;
+                bottom.Visible = true;
+                errTop.Text = ""; errBottom.Text = "";
+                
+              double change_size = trackBar1.Value;
+              pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+              using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+              {
+
+                Pen wPen = new Pen(Color.Black, 2);
+                Brush brush1 = Brushes.White;
+                Brush brush2 = Brushes.Blue;
+                double pSm = g.DpiX / p;
+                double FE = fe * pSm*change_size;
+                double AD = ad * pSm*change_size;      
+                         
+                float radius = (float)(answer(fe)[0] * pSm*change_size);
+                double Answer = answer(fe)[1];
+
+                PointF p1 = new PointF((float)(center.X - (FE / 2)), (float)(center.Y - (FE / 2)));
+                PointF p2 = new PointF((float)(center.X + (FE / 2)), (float)(center.Y - (FE / 2)));
+                PointF p3 = new PointF((float)(center.X + (AD / 2)), (float)(center.Y + (FE / 2)));
+                PointF p4 = new PointF((float)(center.X - (AD / 2)), (float)(center.Y + (FE / 2)));
+                PointF[] trapeze ={p1,p2,p3,p4};
+
+                g.FillPolygon(brush1, trapeze);
+                g.DrawPolygon(wPen, trapeze);
+
+                PointF pT1 = new PointF((float)(center.X - (FE / 2)), (float)(center.Y - (FE / 2)));
+                PointF pT2 = new PointF((float)(center.X + (FE / 2)), (float)(center.Y - (FE / 2)));
+                PointF pT3 = new PointF(center.X, (float)(center.Y + (FE / 2)));
+                PointF[] triangle ={pT1,pT2,pT3};
+                g.FillPolygon(brush2, triangle);
+                g.DrawPolygon(wPen, triangle);
+                Rectangle rect = new Rectangle((int)(center.X-radius), (int)(center.Y - (FE / 2)), (int)(radius*2), (int)(radius*2));
+                g.FillEllipse(brush1, rect);
+                g.DrawEllipse(wPen, rect);
+
+                textBox3.Text = Answer.ToString() + " см";
+                pictureBox1.Invalidate();
+                pictureBox1.Refresh();
+               
+              }
+           
+
+            }
+        }
+
+        private double [] answer(double side)
+        {
+            double between_result = 1.25 * side * side;
+            double c = Math.Sqrt(between_result);
+            double square_tr = 0.5 * side * side;
+            double r = square_tr / ((c + c + side) / 2);
+            double square_r = Math.PI * r * r;
+            double answer = Math.Round(square_tr - square_r, 2);
+            return new double [] { r, answer };
+        }
+
+        private double checkNumber(string str)
+        {
+            double num;
+            if(!Double.TryParse(str, out num))
+            {
+                return -2;
+            }
+            else if (Double.Parse(str) < 0 || Double.Parse(str) > 100)
+            {
+                return -1;
+            }
+            else
+            {
+                return Double.Parse(str);
+            }
+        }
+
+        
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        
+    }
+}
